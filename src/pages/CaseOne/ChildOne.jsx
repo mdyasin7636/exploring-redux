e32889fe98feaf02e830b99e508ecfbcfee3cab9
mdyasin111 <mdyasin7636@gmail.com>
2023-08-22 22:00:28 +0600

    updated

--- src/pages/CaseOne/ChildOne.jsx
+++ src/pages/CaseOne/ChildOne.jsx
@@ -1,4 +1,6 @@
-const ChildOne = () => {
+
+const ChildOne = ({count, setCount}) => {
+  
   return (
     <div className="border-2 border-purple-600 m-10 max-w-4xl mx-auto rounded-md">
       <div className="bg-purple-300 w-10 h-10 m-5 rounded-full text-3xl grid place-content-center">
@@ -6,9 +8,17 @@ const ChildOne = () => {
       </div>
       <div className="w-full grid place-content-center">
         <div className="flex shrink-0 px-10 m-10 gap-5 rounded-xl max-w-md justify-center items-center">
-          <button className="bg-red-300 py-2 px-5 rounded-md">Decrement</button>
-          <h1>0</h1>
-          <button className="bg-emerald-300 py-2 px-5 rounded-md">
+          <button
+            onClick={() => setCount((prev) => prev - 1)}
+            className="bg-red-300 py-2 px-5 rounded-md"
+          >
+            Decrement
+          </button>
+          <h1>{count}</h1>
+          <button
+            onClick={() => setCount((prev) => prev + 1)}
+            className="bg-emerald-300 py-2 px-5 rounded-md"
+          >
             Increment
           </button>
         </div>
