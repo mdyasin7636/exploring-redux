3d46bd1c5beb38b433b8eca9e89bd1e92539149b
mdyasin111 <mdyasin7636@gmail.com>
2023-08-23 18:19:27 +0600

    redux toolkit used

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,9 @@
 // import { useContext } from "react";
 // import { COUNTER_CONTEXT } from "../context/CounterContext";
 
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByValue } from "../redux/features/counter/counterSlice";
+
 // const Counter = () => {
 
 //   const {count, setCount} = useContext(COUNTER_CONTEXT);
@@ -28,9 +31,21 @@
 
 
 const Counter = () => {
+
+  const {count} = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  console.log(count);
+
   return (
     <div>
-      
+      <button onClick={() => dispatch(incrementByValue(5))}>Increment By 5</button>
+      <br />
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <div>
+        <h1>{count}</h1>
+      </div>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 };
