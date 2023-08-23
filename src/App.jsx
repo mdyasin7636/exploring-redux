32053f286a9e8841639ed71f195f40ef14606adf
mdyasin111 <mdyasin7636@gmail.com>
2023-08-23 10:52:35 +0600

    redux toolkit setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 // import CaseOne from "./pages/CaseOne/CaseOne";
-import CounterContext from "./context/CounterContext";
-import CaseTwo from "./pages/CaseTwo/CaseTwo";
+// import CounterContext from "./context/CounterContext";
+// import CaseTwo from "./pages/CaseTwo/CaseTwo";
+
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
-    <CounterContext>
-      {/* <CaseOne/> */}
-      <CaseTwo/>
-    </CounterContext>
+    // <CounterContext>
+    //   <CaseOne/>
+    //   <CaseTwo/>
+    // </CounterContext>
+    <Home/>
   );
 };
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,25 +1,36 @@
-import { useContext } from "react";
-import { COUNTER_CONTEXT } from "../context/CounterContext";
+// import { useContext } from "react";
+// import { COUNTER_CONTEXT } from "../context/CounterContext";
+
+// const Counter = () => {
+
+//   const {count, setCount} = useContext(COUNTER_CONTEXT);
+ 
+//   return (
+//     <div className="flex shrink-0 p-10 m-10 gap-5 bg-gray-100 rounded-xl max-w-md justify-center items-center">
+//       <button
+//         onClick={() => setCount((prev) => prev - 1)}
+//         className="bg-red-300 py-2 px-5 rounded-md"
+//       >
+//         Decrement
+//       </button>
+//       <h1>{count}</h1>
+//       <button
+//         onClick={() => setCount((prev) => prev + 1)}
+//         className="bg-emerald-300 py-2 px-5 rounded-md"
+//       >
+//         Increment
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Counter;
+
 
 const Counter = () => {
-
-  const {count, setCount} = useContext(COUNTER_CONTEXT);
- 
   return (
-    <div className="flex shrink-0 p-10 m-10 gap-5 bg-gray-100 rounded-xl max-w-md justify-center items-center">
-      <button
-        onClick={() => setCount((prev) => prev - 1)}
-        className="bg-red-300 py-2 px-5 rounded-md"
-      >
-        Decrement
-      </button>
-      <h1>{count}</h1>
-      <button
-        onClick={() => setCount((prev) => prev + 1)}
-        className="bg-emerald-300 py-2 px-5 rounded-md"
-      >
-        Increment
-      </button>
+    <div>
+      
     </div>
   );
 };
